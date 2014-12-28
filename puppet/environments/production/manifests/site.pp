0a2bd1a51719c93d4c79afc3e151c01cffbe7758
Nathan R Valentine <nrvale0@gmail.com>
2014-12-27 20:35:51 -0800

    allow noop twiddle from PE Console due to pick()

--- puppet/environments/production/manifests/site.pp
+++ puppet/environments/production/manifests/site.pp
@@ -1,14 +1,14 @@
 filebucket { 'main':
-  server => $::servername,
-  path   => false,
+    server => $::servername,
+      path   => false,
 }
 
 File { backup => 'main' }
 
-$force_noop = hiera('force_noop')
-unless false == $force_noop {
-  notify { "Puppet noop safety latch is enabled in site.pp!": }
-  noop()
+$force_noop_real = str2bool( pick( $::force_noop, hiera('force_noop', 'true') ) )
+if $force_noop_real {
+    notify { "Puppet noop safety latch is enabled in site.pp!": }
+      noop()
 }
 
 hiera_include('classes')
